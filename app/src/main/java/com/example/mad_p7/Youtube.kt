dc3ec7f4d5b64668713c87dd3e73db04641d294d
student <bhargavdodiya21@gnu.ac.in>
2023-09-12 10:40:26 +0530

    k1.1

--- app/src/main/java/com/example/mad_p7/Youtube.kt
+++ app/src/main/java/com/example/mad_p7/Youtube.kt
@@ -2,10 +2,29 @@ package com.example.mad_p7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class Youtube : AppCompatActivity() {
+    private val webView: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+        val webView = findViewById<WebView>(R.id.webviwe)
+        webView.webViewClient = WebViewClient()
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+       webSettings.loadWithOverviewMode=true
+        webSettings.useWideViewPort=true
+        webView.loadUrl("https://youtube.com/embed/fjn9B64Znrk")
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
